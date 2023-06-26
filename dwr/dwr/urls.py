1282a176e0d6b861048ddac89f09c1e3660bb805
ASP <andrewpeshekhonov@gmail.com>
2023-06-26 14:46:07 +0300

    swagger yasg

--- dwr/dwr/urls.py
+++ dwr/dwr/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('api/v1/', include('core.urls_api')),
 ]
+
+urlpatterns = doc_urls + urlpatterns
